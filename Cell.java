aee1e6377b9284f49abf2e6ca7f3e4f88212e160
Conner Toney <conrad.toney@gmail.com>
2015-01-21 13:56:05 -0500

    enum

---
+++ Cell.java
@@ -0,0 +1,10 @@
+/**********************************************************************
+Enumerated class used for the tiles on the board game.
+
+@author Conner Toney
+@version GVSU Fall 2014
+ *********************************************************************/
+package package1;
+
+public enum Cell
+{X, O, EMPTY}
